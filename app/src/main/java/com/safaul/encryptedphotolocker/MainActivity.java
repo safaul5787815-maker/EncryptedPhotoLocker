0d41a3a2ea562a27294d638d4dd35449842bfc4d
safaul5787815-maker <safaul5787815@gmail.com>
2026-08-20 17:29:30 +0530

    Fix Android file picker

--- app/src/main/java/com/safaul/encryptedphotolocker/MainActivity.java
+++ app/src/main/java/com/safaul/encryptedphotolocker/MainActivity.java
@@ -1,7 +1,11 @@
 package com.safaul.encryptedphotolocker;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -9,6 +13,10 @@ import android.webkit.WebViewClient;
 public class MainActivity extends Activity {
 
     private WebView webView;
+
+    private ValueCallback<Uri[]> filePathCallback;
+
+    private static final int FILE_CHOOSER_REQUEST = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,75 @@ public class MainActivity extends Activity {
 
         webView.setWebViewClient(new WebViewClient());
 
-        webView.loadUrl("file:///android_asset/index.html");
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onShowFileChooser(
+                    WebView webView,
+                    ValueCallback<Uri[]> callback,
+                    FileChooserParams fileChooserParams) {
+
+                if (filePathCallback != null) {
+                    filePathCallback.onReceiveValue(null);
+                }
+
+                filePathCallback = callback;
+
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                intent.setType("*/*");
+
+                startActivityForResult(
+                        intent,
+                        FILE_CHOOSER_REQUEST
+                );
+
+                return true;
+            }
+        });
+
+        webView.loadUrl(
+                "file:///android_asset/index.html"
+        );
 
         setContentView(webView);
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        super.onActivityResult(
+                requestCode,
+                resultCode,
+                data
+        );
+
+        if (requestCode == FILE_CHOOSER_REQUEST) {
+
+            if (filePathCallback == null) {
+                return;
+            }
+
+            Uri[] results = null;
+
+            if (resultCode == RESULT_OK && data != null) {
+
+                Uri uri = data.getData();
+
+                if (uri != null) {
+                    results = new Uri[]{uri};
+                }
+            }
+
+            filePathCallback.onReceiveValue(results);
+
+            filePathCallback = null;
+        }
     }
 
     @Override
